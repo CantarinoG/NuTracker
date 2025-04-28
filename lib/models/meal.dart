1852b230d737320c4d1a3fe2904bed8145304512
CantarinoG <guilhermecantarino1709@gmail.com>
2025-04-28 17:03:51 -0300

    feat(model): add meal model

---
+++ lib/models/meal.dart
@@ -0,0 +1,75 @@
+class Meal {
+  final String name;
+  final DateTime dateTime;
+  final double calories;
+  final double carbohydrates;
+  final double proteins;
+  final double totalFats;
+  final double saturatedFats;
+  final double fiber;
+  final double addedSugar;
+  final double alcohol;
+  final double calcium;
+  final double iron;
+  final double magnesium;
+  final double potassium;
+  final double sodium;
+  final double zinc;
+  final double phosphorus;
+  final double iodine;
+  final double selenium;
+  final double copper;
+  final double manganese;
+  final double chlorine;
+  final double vitaminA;
+  final double vitaminB1;
+  final double vitaminB2;
+  final double vitaminB3;
+  final double vitaminB5;
+  final double vitaminB6;
+  final double vitaminB7;
+  final double vitaminB9;
+  final double vitaminB12;
+  final double vitaminC;
+  final double vitaminD;
+  final double vitaminE;
+  final double vitaminK;
+
+  const Meal({
+    required this.name,
+    required this.dateTime,
+    required this.calories,
+    required this.carbohydrates,
+    required this.proteins,
+    required this.totalFats,
+    required this.saturatedFats,
+    required this.fiber,
+    required this.addedSugar,
+    required this.alcohol,
+    required this.calcium,
+    required this.iron,
+    required this.magnesium,
+    required this.potassium,
+    required this.sodium,
+    required this.zinc,
+    required this.phosphorus,
+    required this.iodine,
+    required this.selenium,
+    required this.copper,
+    required this.manganese,
+    required this.chlorine,
+    required this.vitaminA,
+    required this.vitaminB1,
+    required this.vitaminB2,
+    required this.vitaminB3,
+    required this.vitaminB5,
+    required this.vitaminB6,
+    required this.vitaminB7,
+    required this.vitaminB9,
+    required this.vitaminB12,
+    required this.vitaminC,
+    required this.vitaminD,
+    required this.vitaminE,
+    required this.vitaminK,
+  });
+}
